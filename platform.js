1174b82e3ddbf69723ad01ee164a21e947a5e9f0
Saurabh Talele <Saurabh.Talele@bentley.com>
2024-10-23 15:49:45 +0530

    update

--- platform.js
+++ platform.js
@@ -1,36 +1,36 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const filepath = path.join(__dirname, "test-results.json");
 
-const data = fs.readFileSync(filepath, 'utf8');
+const data = fs.readFileSync(filepath, "utf8");
 const results = JSON.parse(data);
 
-if (results.numFailedTests === 0 &&
-    results.numFailedTestSuites === 0 &&
-    results.numPassedTests > 0 &&
-    results.numPassedTestSuites > 0 &&
-    results.success === true) {
-      for(const result in results.testResults) {
+if (
+  results.numFailedTests === 0 &&
+  results.numFailedTestSuites === 0 &&
+  results.numPassedTests > 0 &&
+  results.numPassedTestSuites > 0 &&
+  results.success === true
+) {
+  for (const test in results.testResults) {
+    // eslint-disable-next-line no-console
+    console.log(test.assertionResults[0].ancestorTitles);
+    for (const assertionResult in test.assertionResults) {
+      if (assertionResult.status !== "passed") {
         // eslint-disable-next-line no-console
-        console.log(result.assertionResults[0].ancestorTitles);
-        for(const test in result.assertionResults) {
-          if(test.status !== "passed") {
-            // eslint-disable-next-line no-console
-            console.error(`Test failed: ${test.fullName}`);
-            process.exit(1);
-        }
-        else{
-          // eslint-disable-next-line no-console
-          console.log(`Test passed: ${test.fullName}`);
-        }
+        console.error(`Test failed: ${assertionResult.fullName}`);
+        process.exit(1);
+      } else {
+        // eslint-disable-next-line no-console
+        console.log(`Test passed: ${test.fullName}`);
       }
     }
-    // eslint-disable-next-line no-console
-    console.log("All tests passed");
   }
-  else {
-    // eslint-disable-next-line no-console
-    console.log("Some tests failed");
-    process.exit(1);
-  }
+  // eslint-disable-next-line no-console
+  console.log("All tests passed");
+} else {
+  // eslint-disable-next-line no-console
+  console.log("Some tests failed");
+  process.exit(1);
+}
