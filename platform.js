17470f79cf842dfe3cff8ec65dac67333768a4c7
Saurabh Talele <Saurabh.Talele@bentley.com>
2024-10-23 11:11:25 +0530

    test

--- platform.js
+++ platform.js
@@ -1,1 +1,15 @@
-console.log("Passing")
+const fs = require('fs');
+const path = require('path');
+
+const filepath = path.join(__dirname, "test-results.json");
+
+const data = fs.readFileSync(filepath, 'utf8');
+const results = JSON.parse(data);
+
+if(results.numFailedTests === 0 && results.numFailedTestSuites === 0) {
+  console.log("All tests passed");
+}
+else{
+    console.log("Some tests failed");
+    process.exit(1);
+}
