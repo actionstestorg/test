917839883bf8a3816932a63168671a03df75da28
Saurabh Talele <Saurabh.Talele@bentley.com>
2024-10-25 17:29:13 +0530

    tsest

--- platform.js
+++ platform.js
@@ -1,47 +1,58 @@
 const fs = require("fs");
 const path = require("path");
 
-const filepath = path.join(__dirname, "test-results.json");
-console.log(filepath)
-
-const data = fs.readFileSync(filepath, "utf8");
-const results = JSON.parse(data);
-
-for (let tests in results.testResults) {
-  // eslint-disable-next-line no-console
-  console.log(
-    results.testResults[tests].assertionResults?.[0].ancestorTitles[0]
-  );
-  for (const assertionResult in results.testResults[tests].assertionResults) {
-    if (
-      results.testResults[tests].assertionResults[assertionResult].status ===
-      "passed"
-    ) {
-      // eslint-disable-next-line no-console
-      console.log(
-        `\tTest passed: ${results.testResults[tests].assertionResults[assertionResult].title}`
-      );
-    } else if (
-      results.testResults[tests].assertionResults[assertionResult].status ===
-      "pending"
-    ) {
-      // eslint-disable-next-line no-console
-      console.log(
-        `\tTest skipped: ${results.testResults[tests].assertionResults[assertionResult].title}`
-      );
-    } else {
-      // eslint-disable-next-line no-console
-      console.log(
-        `\tTest failed: ${results.testResults[tests].assertionResults[assertionResult].title}`
-      );
-      process.exit(1);
-    }
+const verifyTestResults = () => {
+  const filepath = path.join(__dirname, "packages", "drawing-production", "test-results.json");
+  
+  try {
+    const fileData = fs.readFileSync(filepath, "utf8");
+    const results = JSON.parse(fileData);
+  } catch (err) {
+    console.log("Error reading test results file");
+    process.exit(1);
   }
 }
-console.log("Summary:");
-console.log(`\tTotal tests: ${results.numTotalTests}`);
-console.log(`\tTotal passed: ${results.numPassedTests}`);
-console.log(`\tTotal failed: ${results.numFailedTests}`);
-console.log(`\tTotal skipped: ${results.numPendingTests}`);
-console.log(`\tTotal suites: ${results.numTotalTestSuites}`);
-console.log(`\tTotal passed: ${results.numPassedTestSuites}`);
+
+verifyTestResults()
+
+// const data = fs.readFileSync(filepath, "utf8");
+// const results = JSON.parse(data);
+
+// for (let tests in results.testResults) {
+//   // eslint-disable-next-line no-console
+//   console.log(
+//     results.testResults[tests].assertionResults?.[0].ancestorTitles[0]
+//   );
+//   for (const assertionResult in results.testResults[tests].assertionResults) {
+//     if (
+//       results.testResults[tests].assertionResults[assertionResult].status ===
+//       "passed"
+//     ) {
+//       // eslint-disable-next-line no-console
+//       console.log(
+//         `\tTest passed: ${results.testResults[tests].assertionResults[assertionResult].title}`
+//       );
+//     } else if (
+//       results.testResults[tests].assertionResults[assertionResult].status ===
+//       "pending"
+//     ) {
+//       // eslint-disable-next-line no-console
+//       console.log(
+//         `\tTest skipped: ${results.testResults[tests].assertionResults[assertionResult].title}`
+//       );
+//     } else {
+//       // eslint-disable-next-line no-console
+//       console.log(
+//         `\tTest failed: ${results.testResults[tests].assertionResults[assertionResult].title}`
+//       );
+//       process.exit(1);
+//     }
+//   }
+// }
+// console.log("Summary:");
+// console.log(`\tTotal tests: ${results.numTotalTests}`);
+// console.log(`\tTotal passed: ${results.numPassedTests}`);
+// console.log(`\tTotal failed: ${results.numFailedTests}`);
+// console.log(`\tTotal skipped: ${results.numPendingTests}`);
+// console.log(`\tTotal suites: ${results.numTotalTestSuites}`);
+// console.log(`\tTotal passed: ${results.numPassedTestSuites}`);
