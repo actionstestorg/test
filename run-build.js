bd0154ec4c9bf0d62baf2a58d99a3859beede2dc
YashodipD <113993668+YashodipD@users.noreply.github.com>
2024-11-05 21:32:31 +0530

    mem workflow

---
+++ run-build.js
@@ -0,0 +1,25 @@
+const { exec } = require("child_process");
+
+const v8 = require("v8");
+const buildCommand = "pnpm change";
+
+const child = exec(buildCommand, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Stdout: ${stdout}`);
+});
+
+child.on("exit", () => {
+  const memoryUsage = process.memoryUsage();
+  console.log(`RSS: ${memoryUsage.rss}`);
+  console.log(`Heap Total: ${memoryUsage.heapTotal}`);
+  console.log(`Heap Used: ${memoryUsage.heapUsed}`);
+  console.log(`External: ${memoryUsage.external}`);
+  console.log(`Heap Info: ${JSON.stringify(v8.getHeapStatistics())}`);
+});
